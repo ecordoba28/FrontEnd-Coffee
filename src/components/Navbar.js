1e3238b6d38689e4afdac7222888895f4b6626da
Ezequiel <isaacgtfo@gmail.com>
2022-06-07 17:59:25 -0300

    primer commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">FrontEnd Coffee-Shop</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html"></a></li>
+        <li><a href="collapsible.html">Contacto</a></li>
+      </ul>
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
